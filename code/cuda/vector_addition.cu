991954aa984b5d1b02d8b932895a2117c421fbc9
Melissa M <melissafm24@gmail.com>
2018-01-05 14:20:24 -0500

    fix indentation

--- code/cuda/vector_addition.cu
+++ code/cuda/vector_addition.cu
@@ -22,21 +22,21 @@ int main(void) {
     /////////////////////
 
     // host copies of a, b, c
-	int *a, *b, *c;
+    int *a, *b, *c;
     // device copies of a, b, c
-	int *d_a, *d_b, *d_c;
-	int size = N * sizeof(int);
+    int *d_a, *d_b, *d_c;
+    int size = N * sizeof(int);
 
-	// Alloc space for device copies of a, b, c
-	cudaMalloc((void **)&d_a, size);
-	cudaMalloc((void **)&d_b, size);
-	cudaMalloc((void **)&d_c, size);
+    // Alloc space for device copies of a, b, c
+    cudaMalloc((void **)&d_a, size);
+    cudaMalloc((void **)&d_b, size);
+    cudaMalloc((void **)&d_c, size);
 
-	// Alloc space for host copies of a, b, c
-	// Setup input values
-	a = (int *)malloc(size); random_ints(a, N);
-	b = (int *)malloc(size); random_ints(b, N);
-	c = (int *)malloc(size);
+    // Alloc space for host copies of a, b, c
+    // Setup input values
+    a = (int *)malloc(size); random_ints(a, N);
+    b = (int *)malloc(size); random_ints(b, N);
+    c = (int *)malloc(size);
 
     for(int i=0; i< N; i++)
     {
@@ -44,23 +44,23 @@ int main(void) {
                 a[i], b[i], a[i] + b[i]);
     }
 
-	// Copy inputs to device
-	cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+    // Copy inputs to device
+    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
 
-	// Launch add() kernel on GPU with N blocks
-	add<<<N,1>>>(d_a, d_b, d_c);
+    // Launch add() kernel on GPU with N blocks
+    add<<<N,1>>>(d_a, d_b, d_c);
 
-	// Copy result back to host
-	cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+    // Copy result back to host
+    cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
 
     for(int i=0; i< N; i++)
     {
         printf("Vector Addition Result : %d \n", c[i]);
     }
 
-	// Cleanup
-	free(a); free(b); free(c);
-	cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
-	return 0;
+    // Cleanup
+    free(a); free(b); free(c);
+    cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+    return 0;
 }
