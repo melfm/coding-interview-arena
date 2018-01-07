639ebd32fcc7dad43e9f1a27767977e94071803f
Melissa Mozifian <melissafm24@gmail.com>
2018-01-07 16:54:38 -0500

    add matrix rotation

--- code/python/misc_questions_test.py
+++ code/python/misc_questions_test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import unittest
 
 import misc_questions as misc
@@ -15,6 +16,34 @@ class StringManipulationTest(unittest.TestCase):
 
         self.assertTrue(misc.isPowerOf3_recursive(27))
         self.assertFalse(misc.isPowerOf3_recursive(10))
+
+    def test_rotate_matrix(self):
+
+        dummy_matrix = np.random.rand(5, 5)
+        print('Original matrix')
+        print(dummy_matrix)
+
+        rotated_matrix = misc.rotate_matrix(dummy_matrix)
+        print('Rotated matrix by 90')
+        print(rotated_matrix)
+
+        dummy_matrix = np.random.rand(5, 6)
+        print('Original matrix')
+        print(dummy_matrix)
+
+        rotated_matrix = misc.rotate_matrix(dummy_matrix)
+        print('Rotated matrix by 90')
+        print(rotated_matrix)
+
+    def test_rotate_matrix_in_place(self):
+
+        dummy_matrix = np.random.rand(5, 5)
+        print('Original matrix')
+        print(dummy_matrix)
+
+        rotated_matrix = misc.rotate_matrix_in_place(dummy_matrix, 5)
+        print('Rotated matrix by 90')
+        print(rotated_matrix)
 
 
 if __name__ == '__main__':
