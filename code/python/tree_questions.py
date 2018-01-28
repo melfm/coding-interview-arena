0c5e574bb82203c7156f78497da8b53c2f45e89d
Melissa Mozifian <melissafm24@gmail.com>
2018-01-27 23:56:01 -0500

    generic tree class with basic add/traversal

--- code/python/tree_questions.py
+++ code/python/tree_questions.py
@@ -1,91 +1,53 @@
-"""Tree Questions."""
+"""Generic Tree class."""
+
+
+class Child:
+
+    def __init__(self, value):
+        self.node = TreeNode(value)
+        self.next = None
+
+    def add_child(self, node):
+        if self.next is None:
+            self.next = Child(node)
+        else:
+            self.next.add_child(node)
+
+    def print_children(self, child, all_values):
+
+        if child is None:
+            return
+
+        all_values.append(child.node.value)
+
+        if child.next is None:
+            return
+        self.print_children(child.next,
+                            all_values)
 
 
 class TreeNode:
-    """Binary Tree traversal.
-    """
+    """Generic tree class."""
 
-    def __init__(self, value):
-
-        self.left = None
-        self.right = None
+    def __init__(self, value, children=None):
         self.value = value
+        self.children = children
 
-    def insert_right(self, node, value):
-        if node.right is None:
-            self.right = TreeNode(value)
+    def add_children(self, node_values):
 
-        else:
-            new_node = TreeNode(value)
-            new_node.left = self.left
-            new_node.right = node
+        for val in node_values:
+            if self.children is None:
+                self.children = Child(val)
+            else:
+                self.children.add_child(val)
 
-    def insert_left(self, node, value):
-        if node.left is None:
-            self.left = TreeNode(value)
-        else:
-            new_node = TreeNode(value)
-            new_node.left = None
-            new_node.right = None
-
-    def traverse_inorder(self, node, children_list=[]):
-        # In-Order: Traverse left node, current node, then right
+    def traverse_tree(self, node, all_values=[]):
 
         if node is None:
-            return children_list
+            return
+        all_values.append(node.value)
 
-        self.traverse_inorder(node.left, children_list)
-        children_list.append(node.value)
-        self.traverse_inorder(node.right, children_list)
-
-    def traverse_preorder(self, node, children_list=[]):
-        # Pre-Order: Traverse current node, then left node,
-        # then right node
-
-        if node is None:
-            return children_list
-
-        children_list.append(node.value)
-        self.traverse_preorder(node.left, children_list)
-        self.traverse_preorder(node.right, children_list)
-
-    def traverse_postorder(self, node, children_list=[]):
-        # Post-Order: Traverse left node, then right node,
-        # then current node.
-
-        if node is None:
-            return children_list
-
-        self.traverse_postorder(node.left, children_list)
-        self.traverse_postorder(node.right, children_list)
-        children_list.append(node.value)
-
-    def max_depth(self, node):
-        # Find the max depth of a binary tree
-        if node is None:
-            return 0
-
-        return 1 + max(self.max_depth(node.right),
-                       self.max_depth(node.left))
-
-    def min_depth(self, node):
-        # Find the min depth of a binary tree
-        if node is None:
-            return 0
-
-        return 1 + min(self.min_depth(node.right),
-                       self.min_depth(node.left))
-
-    def is_tree_balanced(self, node):
-        # Find whether a binary tree is balanced or not
-
-        if node is None:
-            return True
-
-        height_diff = self.max_depth(node.left) - self.max_depth(node.right)
-
-        if abs(height_diff) > 1:
-            return False
-        else:
-            return self.is_tree_balanced(node.left) and\
-                self.is_tree_balanced(node.right)
+        if node.children is None:
+            return
+        self.traverse_tree(node.children.print_children(node.children,
+                                                        all_values))
--- code/python/tree_questions_test.py
+++ code/python/tree_questions_test.py
@@ -7,117 +7,26 @@ class TreeQuestionTest(unittest.TestCase):
 
     dump_output = True
 
-    @classmethod
-    def setUpClass(cls):
-        cls.root = trees.TreeNode(1)
+    def test_tree_traversal(self):
+        root_node = trees.TreeNode(1)
 
-        cls.root.left = trees.TreeNode(2)
-        cls.root.right = trees.TreeNode(3)
+        root_node.add_children([4, 3])
 
-        cls.root.left.left = trees.TreeNode(4)
-        cls.root.left.right = trees.TreeNode(5)
-        cls.root.right.left = trees.TreeNode(6)
-        cls.root.right.right = trees.TreeNode(7)
+        root_node.children.add_child(2)
 
-    def test_in_order_traversal(self):
+        root_node.children.add_child(5)
+        root_node.children.next.add_child(8)
+
+        exp_all_children = [1, 4, 3, 2, 5, 8]
 
         all_children = []
-        self.root.traverse_inorder(self.root,
-                                   all_children)
-
-        exp_child_values = [4, 2, 5, 1, 6, 3, 7]
+        root_node.traverse_tree(root_node,
+                                all_children)
 
         self.assertEqual(all_children,
-                         exp_child_values)
-
+                         exp_all_children)
         if self.dump_output:
             print(all_children)
-
-    def test_pre_order_traversal(self):
-
-        all_children = []
-        self.root.traverse_preorder(self.root,
-                                    all_children)
-
-        exp_child_values = [1, 2, 4, 5, 3, 6, 7]
-
-        self.assertEqual(all_children,
-                         exp_child_values)
-
-        if self.dump_output:
-            print(all_children)
-
-    def test_post_order_traversal(self):
-
-        all_children = []
-        self.root.traverse_postorder(self.root,
-                                     all_children)
-
-        exp_child_values = [4, 5, 2, 6, 7, 3, 1]
-
-        self.assertEqual(all_children,
-                         exp_child_values)
-
-        if self.dump_output:
-            print(all_children)
-
-    def test_find_max_depth(self):
-        root = trees.TreeNode(1)
-
-        root.left = trees.TreeNode(2)
-        root.right = trees.TreeNode(3)
-
-        root.left.left = trees.TreeNode(4)
-        root.left.right = trees.TreeNode(5)
-        root.right.left = trees.TreeNode(6)
-        root.right.right = trees.TreeNode(7)
-
-        root.right.right.left = trees.TreeNode(9)
-        root.right.right.right = trees.TreeNode(8)
-        root.right.right.left.right = trees.TreeNode(10)
-
-        tree_depth = root.max_depth(root)
-
-        self.assertEqual(tree_depth, 5)
-
-    def test_find_min_depth(self):
-        root = trees.TreeNode(1)
-
-        root.left = trees.TreeNode(2)
-        root.right = trees.TreeNode(3)
-
-        root.left.left = trees.TreeNode(4)
-        root.left.right = trees.TreeNode(5)
-        root.right.left = trees.TreeNode(6)
-        root.right.right = trees.TreeNode(7)
-
-        root.right.right.left = trees.TreeNode(9)
-        root.right.right.right = trees.TreeNode(8)
-        root.right.right.left.right = trees.TreeNode(10)
-
-        tree_depth = root.min_depth(root)
-
-        self.assertEqual(tree_depth, 3)
-
-    def test_is_tree_balanced(self):
-        root = trees.TreeNode(1)
-
-        root.left = trees.TreeNode(2)
-        root.right = trees.TreeNode(3)
-
-        root.left.left = trees.TreeNode(4)
-        root.left.right = trees.TreeNode(5)
-        root.right.left = trees.TreeNode(6)
-        root.right.right = trees.TreeNode(7)
-
-        is_balanced = root.is_tree_balanced(root)
-        self.assertEqual(is_balanced, True)
-
-        root.right.right.left = trees.TreeNode(9)
-        root.right.right.right = trees.TreeNode(8)
-        root.right.right.left.right = trees.TreeNode(10)
-        is_balanced = root.is_tree_balanced(root)
-        self.assertEqual(is_balanced, False)
 
 
 if __name__ == '__main__':
