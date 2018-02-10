298d0a58a680513d3ea0bd740d6d6660094f0e89
Melissa Mozifian <melissafm24@gmail.com>
2018-02-09 22:03:43 -0500

    add string anagram Q

--- code/python/string_manipulation.py
+++ code/python/string_manipulation.py
@@ -1,5 +1,4 @@
 
-
 def reverse_string(string):
 
     return string[::-1]
@@ -54,3 +53,57 @@ def return_palindrome_subset(word):
         p1 -= 1
 
     return word
+
+
+def string_anagram_loop(string_a, string_b):
+    """Write a method to decide if two strings
+    are anagrams or not."""
+
+    string_a_list = list(string_a)
+    string_b_list = list(string_b)
+
+    if len(string_a) != len(string_b):
+        return False
+
+    match_found = False
+
+    for i in string_a_list:
+        current_char_a = i
+        match_found = False
+        for j in string_b_list:
+            current_char_b = j
+
+            if current_char_a == current_char_b:
+                match_found = True
+
+            else:
+                continue
+
+        if not match_found:
+            break
+
+    return match_found
+
+
+def string_anagram_count(string_a, string_b):
+    """Decides if two strings are anagrams or not
+    by keeping a char counter to compare with."""
+
+    num_of_chars = 256
+    count_a = [0] * num_of_chars
+    count_b = [0] * num_of_chars
+
+    if len(string_a) != len(string_b):
+        return False
+
+    for i in string_a:
+        count_a[ord(i)] += 1
+
+    for i in string_b:
+        count_b[ord(i)] += 1
+
+    for i in range(num_of_chars):
+        if count_a[i] != count_b[i]:
+            return False
+
+    return True
