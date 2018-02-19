39412751e03f413048f61694a452d40664e5cba3
Melissa M <melissafm24@gmail.com>
2018-02-19 14:46:41 -0500

    add basic linked list questions

---
+++ code/python/linked_list_questions.py
@@ -0,0 +1,97 @@
+"""Linked List Questions."""
+
+
+class Node:
+
+    def __init__(self, value):
+
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def push(self, value):
+
+        if self.head is None:
+            self.head = Node(value)
+            self.next = None
+
+        else:
+            new_node = Node(value)
+            new_node.next = self.head
+            self.head = new_node
+
+    def pop(self):
+
+        if self.head is None:
+            return
+
+        head = self.head
+        self.head = self.head.next
+        return head.value
+
+    def print_nodes(self):
+        head = self.head
+        while(head):
+            print('Node val: ', head.value)
+            head = head.next
+
+    def print_list(self):
+
+        print('Linked List ->')
+        self.detect_cycle(print_values=True)
+        print('--------------')
+
+    def delete(self, value):
+        # TODO
+        if self.head.value == value:
+            tmp_value = self.head.value
+            self.head.value = None
+            self.head = self.head.next
+            return tmp_value
+
+        current = self.head.next
+        while(current):
+            if current.value == value:
+                tmp_value = current.value
+                # do_something
+
+    def detect_cycle(self, print_values=False):
+        """Returns true if there is a cycle.
+        Store the nodes in a set and do a look up.
+        Alternative approach is to store an additional
+        visited flag per node.
+        """
+
+        table = set()
+        current = self.head
+
+        while(current):
+
+            if current in table:
+                return True
+
+            table.add(current)
+            if print_values:
+                print('Node val: ', current.value)
+            current = current.next
+
+        return False
+
+    def detect_cycle_v2(self):
+        """Floyd’s Cycle-Finding algorithm."""
+
+        slow_pointer = self.head
+        fast_pointer = self.head
+
+        while(slow_pointer and fast_pointer and fast_pointer.next):
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+
+            if slow_pointer == fast_pointer:
+                return True
+        return False
---
+++ code/python/linked_list_questions_test.py
@@ -0,0 +1,53 @@
+import unittest
+
+import linked_list_questions as linked_list
+
+
+class LinkedListQuestionsTest(unittest.TestCase):
+
+    dump_output = True
+
+    def test_insert(self):
+
+        test_list = linked_list.LinkedList()
+        test_list.push(2)
+        test_list.push(5)
+        test_list.push(7)
+
+        if self.dump_output:
+            test_list.print_list()
+
+        test_list.pop()
+        test_list.pop()
+        if self.dump_output:
+            test_list.print_list()
+
+    def test_detect_cycle(self):
+        test_list = linked_list.LinkedList()
+        test_list.push(2)
+        test_list.push(5)
+        test_list.push(7)
+
+        self.assertFalse(test_list.detect_cycle())
+        self.assertFalse(test_list.detect_cycle_v2())
+
+        # Manually create a cycle for testing
+        test_list.head.next.next.next = test_list.head
+
+        self.assertTrue(test_list.detect_cycle())
+        self.assertTrue(test_list.detect_cycle_v2())
+
+        # Add more elemenets
+        test_list.push(10)
+        test_list.push(9)
+        test_list.push(18)
+        if self.dump_output:
+            print('Test case detect cycle')
+            test_list.print_list()
+
+        self.assertTrue(test_list.detect_cycle())
+        self.assertTrue(test_list.detect_cycle_v2())
+
+
+if __name__ == '__main__':
+    unittest.main()
