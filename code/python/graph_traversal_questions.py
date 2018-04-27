c3b092f47c062b0e9bda9a00c19b01a00726dee4
Melissa Mozifian <melissafm24@gmail.com>
2018-04-27 10:00:41 -0400

    fix the find path q

--- code/python/graph_traversal_questions.py
+++ code/python/graph_traversal_questions.py
@@ -1,4 +1,5 @@
 """Graph traversal questions."""
+import pdb
 
 def dfs(graph, start_node):
     """Depth-first search."""
@@ -45,3 +46,111 @@ def bfs(graph, start_node):
             for neighbor in neighbors:
                 queue.append(neighbor)
     return visited
+
+
+def path_exist(graph, start_node, node_a, node_b,
+               visited=[], first_node_found=False,
+               all_paths=[]):
+    """ Q1: You are provided with network dataset, composed of nodes
+    and one-directional links. Check whether a route exists between
+    two specific nodes in the network.
+    """
+
+    stack = [start_node]
+
+    if start_node == node_a:
+        first_node_found = True
+
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.append(node)
+
+            for current_node in graph[start_node]:
+                if not first_node_found:
+                    if current_node == node_a:
+                        first_node_found = True
+                if first_node_found:
+                    if current_node == node_b:
+                        path_found = True
+                        all_paths.append(path_found)
+                        print('Found a path.')
+
+                if current_node in graph:
+                    path_exist(graph, current_node, node_a, node_b,
+                               visited, first_node_found,
+                               all_paths)
+
+    return all_paths
+
+
+def dfs_possible_routes(graph, node_a, node_b):
+
+    stack = [(node_a, [node_b])]
+    #pdb.set_trace()
+
+    while stack:
+        #pdb.set_trace()
+        (vertex, path) = stack.pop()
+        for next in graph[vertex] - set(path):
+            if next == node_b:
+                yield path + [next]
+            else:
+                stack.append((next, path + [next]))
+
+
+def bfs_paths2(graph, start, goal):
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                queue.append((next, path + [next]))
+
+
+def shortest_path(graph, start, goal):
+    try:
+        return next(bfs_paths2(graph, start, goal))
+    except StopIteration:
+        return None
+
+
+def path_exist_bfs(graph, start_node, node_a, node_b):
+
+    visited = []
+    queue = [start_node]
+
+    first_node_found = False
+    no_further_neigh = False
+
+    if start_node == node_a:
+        print('Found first node')
+        first_node_found = True
+
+    while queue:
+        print('Queue ->' ,queue)
+        node = queue.pop(0)
+
+        if node == node_a:
+            first_node_found = True
+
+        if node not in visited:
+            visited.append(node)
+            if first_node_found:
+
+                if node == node_b:
+                    return True
+
+
+            if node in graph:
+                neighbors = graph[node]
+                for neighbor in neighbors:
+                    queue.append(neighbor)
+            else:
+                print('Status of queue ', queue)
+                no_further_neigh = True
+
+    return False
+
--- code/python/graph_traversal_questions_test.py
+++ code/python/graph_traversal_questions_test.py
@@ -68,6 +68,62 @@ class GraphTraversalQuestionsTest(unittest.TestCase):
         if self.dump_output:
             print('Depth First Search -> ', nodes)
 
+    def test_path_exist(self):
+
+        graph_data = {1: [2, 3],
+                      2: [4, 5, 6],
+                      3: [7, 8, 9],
+                      8: [10]}
+
+        path_found = graph.path_exist(graph_data, 1, 1, 6)
+        print('Path found? ', path_found[0])
+
+        path_found = graph.path_exist(graph_data, 1, 3, 4, [], False, [])
+
+        if not path_found:
+            print('No path found')
+
+
+    def test_path_exist2(self):
+
+        #self.assertEqual(len(path_found), 0)
+
+        """
+        path_found = graph.path_exist_bfs(graph_data, 1, 3, 4)
+        self.assertFalse(path_found)
+
+        path_found = graph.path_exist_bfs(graph_data, 1, 4, 10)
+        self.assertFalse(path_found)
+        """
+
+
+    #@unittest.skip
+    def test_find_path(self):
+        graph_set = {1: set([2, 3]),
+                2: set([4, 5, 6]),
+                3: set([7, 8, 9]),
+                8: set([10])}
+
+        graph_set = {1: set([2, 3]),
+                2: set([4]),
+                3: set([]),
+                4: set([])}
+
+
+        paths = graph.dfs_possible_routes(graph_set, 1, 4)
+
+        for path in paths:
+            print('Paths found ##########3', path)
+
+        paths = graph.shortest_path(graph_set, 1, 2)
+
+        #for path in paths:
+        #print('Shortest Paths found ', path)
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
