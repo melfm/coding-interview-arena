42ce3f454cf8a8c126a47957c4833331c6952c7b
Melissa Mozifian <melissafm24@gmail.com>
2022-12-07 14:33:22 -0500

    clean up VI

--- code/python/reinforcement_learning/value_iteration.py
+++ code/python/reinforcement_learning/value_iteration.py
@@ -11,29 +11,34 @@ states = [0, 1, 2, 3, 4]
 # reward per state
 rewards = [-1, -1, 10, -1, -1]
 gamma = 0.9
-# deterministic
+# deterministic transition prob
 transition_p = 1.0
 iterations = 50
 stopping_threshold = 0.5
 
 
-def value_iteration(states, actions, rewards):
+def lookahead_best_act_value(values, current_s):
+    best_act_val = np.zeros(len(actions))
+    for a in actions:
+        reward = rewards[current_s]
+        # Terminal state
+        if (current_s + a) >= len(states):
+            best_act_val[a] = reward
+        else:
+            best_act_val[a] += transition_p * (reward +
+                                               gamma * values[current_s + a])
+
+    return best_act_val
+
+
+def value_iteration():
 
     new_values = np.zeros(len(states))
 
     for iter in range(iterations):
         delta = 0
         for s in range(len(states)):
-            best_act_val = np.zeros(len(actions))
-            for a in actions:
-                reward = rewards[s]
-                # terminal state
-                if (s + a) >= len(states):
-                    best_act_val[a] = reward
-                else:
-                    best_act_val[a] += transition_p * (
-                        reward + gamma * new_values[s + a])
-
+            best_act_val = lookahead_best_act_value(new_values, s)
             delta = max(delta, abs(max(best_act_val) - new_values[s]))
             new_values[s] = max(best_act_val)
 
@@ -49,21 +54,12 @@ def optimal_policy(values):
     policy = np.zeros([len(states), len(actions)])
 
     for s in range(len(states)):
-        best_act_val = np.zeros(len(actions))
-        for a in actions:
-            reward = rewards[s]
-            # terminal state
-            if (s + a) >= len(states):
-                best_act_val[a] = reward
-            else:
-                best_act_val[a] += transition_p * (reward +
-                                                   gamma * values[s + a])
+        best_act_val = lookahead_best_act_value(values, s)
         best_action = np.argmax(best_act_val)
         policy[s, best_action] = 1.0
 
     print('Optimal Policy ', policy)
     return policy
 
-
-values = value_iteration(states, actions, rewards)
+values = value_iteration()
 policy = optimal_policy(values)
